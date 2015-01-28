325159a719b687a3c75269931aef21c8b07adf9e
lbt05 <lbt0506@gmail.com>
2015-01-28 14:34:22 +0800

    clean

--- app/src/main/java/com/avoscloud/beijing/push/demo/keepalive/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.avoscloud.beijing.push.demo.keepalive;
-
-import com.avos.avoscloud.im.v2.AVIMMessage;
-
-public interface MessageListener {
-
-  public void onMessage(AVIMMessage msg);
-
-}
